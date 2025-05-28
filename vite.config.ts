00557a8dd3f471d07d6b9708cb69a200c8983f80
gcvfernandez <gian0217@gmail.com>
2025-05-29 04:18:58 +0800

    edit vite to fix issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // This is the key fix
+  base: '/', // CORRECT for Netlify
   plugins: [react()],
 });
