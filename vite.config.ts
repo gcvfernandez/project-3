ff0976f5da72f08dcc1053be2121fe9828d57938
gcvfernandez <gian0217@gmail.com>
2025-05-29 04:15:41 +0800

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // This is the key fix
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
